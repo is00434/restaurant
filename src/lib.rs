75e6e68c5569286f51ab491e5cc079db44336720
Gerhard Ziesler <gerhard.ziesler@gmail.com>
2021-05-20 08:57:51 +0200

    refactor: Modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod front_of_house;
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-
-mod front_of_house;
-
-fn main() {
-    front_of_house::hosting::add_to_waitlist();
-    front_of_house::serving::serve_order()
-}
